a64c9e1c922fd212381b3fcd73deb5a8435e6562
Janne Savukoski <janne@savukoski.name>
2012-05-04 16:28:55 +0300

    Restored original copyright to dist

--- dist/dust-full-0.4.3.js
+++ dist/dust-full-0.4.3.js
@@ -1,3 +1,12 @@
+
+//
+// Dust - Asynchronous Templating v0.4.0
+// http://akdubya.github.com/dustjs
+//
+// Copyright (c) 2010, Aleksander Williams
+// Released under the MIT License.
+//
+
 var dust = {};
 
 (function(dust) {
